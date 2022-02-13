cecb49520eb6d71657c1f2bb94b67e58ae7e32be
Adam Klinkosz <adam.klinkosz@onegini.com>
2022-02-13 21:26:09 +0100

    Rename currency converter to indicate that it is a service

--- src/main/java/pl/cleankod/exchange/core/gateway/CurrencyConversionService.java
+++ src/main/java/pl/cleankod/exchange/core/gateway/CurrencyConversionService.java
@@ -4,6 +4,6 @@ import pl.cleankod.exchange.core.domain.Money;
 
 import java.util.Currency;
 
-public interface CurrencyConverter {
+public interface CurrencyConversionService {
     Money convert(Money money, Currency targetCurrency);
 }
--- src/main/java/pl/cleankod/exchange/entrypoint/AccountController.java
+++ src/main/java/pl/cleankod/exchange/entrypoint/AccountController.java
@@ -3,7 +3,7 @@ package pl.cleankod.exchange.entrypoint;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.cleankod.exchange.core.domain.Account;
-import pl.cleankod.exchange.core.gateway.CurrencyConverter;
+import pl.cleankod.exchange.core.gateway.CurrencyConversionService;
 import pl.cleankod.exchange.core.domain.Money;
 import pl.cleankod.exchange.core.gateway.AccountRepository;
 
@@ -19,7 +19,7 @@ public class AccountController {
 
     private final AccountRepository accountRepository;
 
-    private final CurrencyConverter currencyConverter = new CurrencyConverter() {
+    private final CurrencyConversionService currencyConverter = new CurrencyConversionService() {
         private static final BigDecimal PLN_TO_EUR_RATE = BigDecimal.valueOf(4.58d);
         private static final BigDecimal EUR_TO_PLN_RATE = BigDecimal.valueOf(0.22d);
         @Override
