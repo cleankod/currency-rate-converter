2e58580601036c6869f7baed3e84fd11437051fd
Adam Klinkosz <spyro@o2.pl>
2022-03-21 08:18:09 +0100

    Encapsulate null-checking logic

---
+++ src/test/groovy/pl/cleankod/util/PreconditionsRequireNonNullSpecification.groovy
@@ -0,0 +1,30 @@
+package pl.cleankod.util
+
+import spock.lang.Specification
+
+class PreconditionsRequireNonNullSpecification extends Specification {
+    def "should return object since it's not null"() {
+        when:
+        //noinspection GroovyAssignabilityCheck
+        def actualObject = Preconditions.requireNonNull(givenValue)
+
+        then:
+        actualObject == givenValue
+
+        where:
+        givenValue << ["Some value", 1L, Long.valueOf(1L)]
+    }
+
+    def "should not return object due to null value"() {
+        when:
+        //noinspection GroovyAssignabilityCheck
+        Preconditions.requireNonNull(givenValue)
+
+        then:
+        def exception = thrown(NullPointerException)
+        exception.message.startsWith("Given value cannot be null")
+
+        where:
+        givenValue << [null as String, null as Long]
+    }
+}
