46bdca18671a8edcf4dd90344ee8663e33f670dc
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2022-04-26 00:51:58 +0200

    [API circuit breaker] - testing Nbp API fallback

--- src/main/java/pl/cleankod/exchange/provider/nbp/exceptions/NbpAPIException.java
+++ src/main/java/pl/cleankod/exchange/provider/nbp/exceptions/NbpAPIException.java
@@ -1,7 +1,7 @@
 package pl.cleankod.exchange.provider.nbp.exceptions;
 
 public class NbpAPIException extends RuntimeException {
-    private static final String message = "Error connecting to NBP API";
+    public static final String message = "Error connecting to NBP API";
 
     public NbpAPIException() {
         super(message);
