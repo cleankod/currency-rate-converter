383e713b2e9e49a926d9c3fc6c1928a26f480571
Evangelos Vatikiotis <evangelos.vatikiotis@capgemini.com>
2022-04-25 19:33:38 +0200

    [Simple value object serialization (to avoid `value` nested field in JSON)] - implemented custom deserializer to fix failing AccountSpecificationTests

--- src/test/groovy/pl/cleankod/BaseApplicationSpecification.groovy
+++ src/test/groovy/pl/cleankod/BaseApplicationSpecification.groovy
@@ -1,22 +1,41 @@
 package pl.cleankod
 
+import com.fasterxml.jackson.databind.BeanDescription
+import com.fasterxml.jackson.databind.DeserializationConfig
+import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.deser.BeanDeserializerModifier
+import com.fasterxml.jackson.databind.module.SimpleModule
 import org.apache.http.HttpResponse
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.client.methods.HttpUriRequest
 import org.apache.http.impl.client.HttpClientBuilder
 import org.apache.http.util.EntityUtils
+import pl.cleankod.exchange.core.domain.Account
 import pl.cleankod.exchange.entrypoint.model.ApiError
+import pl.cleankod.util.AccountDeserializer
 import spock.lang.Specification
 
 abstract class BaseApplicationSpecification extends Specification {
   private static init = true
   private static final baseUrl = "http://localhost:8080"
-  private static final ObjectMapper objectMapper = new ObjectMapper()
+  private static final ObjectMapper objectMapper = new ObjectMapper();
 
   @SuppressWarnings('unused')
   def setupSpec() {
     if (init) {
+      SimpleModule module = new SimpleModule();
+      module.setDeserializerModifier(new BeanDeserializerModifier() {
+        @Override public JsonDeserializer<?> modifyDeserializer(
+                DeserializationConfig config, BeanDescription beanDesc, JsonDeserializer<?> deserializer) {
+          if (beanDesc.getBeanClass() == Account.class) {
+            return new AccountDeserializer();
+          }
+
+          return deserializer;
+        }});
+
+      objectMapper.registerModule(module);
       ApplicationInitializer.main(new String[0])
       init = false
     }
