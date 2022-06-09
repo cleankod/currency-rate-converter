54dac575595546c02b424652920502bb55952328
piotr.a@practi.net <piotr.a@practi.net>
2022-06-09 10:58:34 +0200

    added logging aspect for nbp client

--- src/main/java/pl/cleankod/ApplicationInitializer.java
+++ src/main/java/pl/cleankod/ApplicationInitializer.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.env.Environment;
 import pl.cleankod.exchange.core.gateway.AccountRepository;
 import pl.cleankod.exchange.core.gateway.CurrencyConversionService;
@@ -19,6 +20,7 @@ import pl.cleankod.exchange.entrypoint.AccountController;
 import pl.cleankod.exchange.entrypoint.ExceptionHandlerAdvice;
 import pl.cleankod.exchange.provider.AccountInMemoryRepository;
 import pl.cleankod.exchange.provider.CurrencyConversionNbpService;
+import pl.cleankod.exchange.provider.configuration.ExchangeRatesNbpClientLoggingAspect;
 import pl.cleankod.exchange.provider.nbp.ExchangeRatesNbpClient;
 
 import java.util.Currency;
@@ -26,6 +28,7 @@ import java.util.Currency;
 @SpringBootConfiguration
 @EnableAutoConfiguration
 @EnableCaching
+@EnableAspectJAutoProxy
 public class ApplicationInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationInitializer.class, args);
@@ -73,6 +76,7 @@ public class ApplicationInitializer {
 
         return new FindAccountAndConvertCurrencyFooUseCase(findAccountAndConvertCurrencyUseCase, findAccountUseCase);
     }
+
     @Bean
     AccountController accountController(FindAccountAndConvertCurrencyFooUseCase findAccountAndConvertCurrencyFooUseCase) {
         return new AccountController(findAccountAndConvertCurrencyFooUseCase);
@@ -81,5 +85,10 @@ public class ApplicationInitializer {
     @Bean
     ExceptionHandlerAdvice exceptionHandlerAdvice() {
         return new ExceptionHandlerAdvice();
+    }
+
+    @Bean
+    ExchangeRatesNbpClientLoggingAspect loggingAspect() {
+        return new ExchangeRatesNbpClientLoggingAspect();
     }
 }
---
+++ src/main/java/pl/cleankod/exchange/provider/configuration/ExchangeRatesNbpClientLoggingAspect.java
@@ -0,0 +1,27 @@
+package pl.cleankod.exchange.provider.configuration;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import pl.cleankod.exchange.provider.nbp.ExchangeRatesNbpClient;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+@Aspect
+public
+class ExchangeRatesNbpClientLoggingAspect {
+
+    private final Logger log = Logger.getLogger(ExchangeRatesNbpClient.class.getName());
+
+    @Pointcut("execution(* pl.cleankod.exchange.provider.nbp.ExchangeRatesNbpClient.*(..))")
+    void externalApiCallMethod() {
+    }
+
+    @Before("externalApiCallMethod()")
+    void logBeforePerformingExternalApiCall(JoinPoint joinPoint) {
+        log.info("Is about to call NBP with parameters: " + Arrays.toString(joinPoint.getArgs()));
+    }
+
+}
