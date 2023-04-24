fdfbc47553caa7286768e72f8ab0b7bb5637564a
Andrei Closca <ext_andrei.closca@spark.net>
2023-04-24 17:46:30 +0300

    Ading REST API docs.

--- src/main/java/pl/cleankod/exchange/entrypoint/AccountController.java
+++ src/main/java/pl/cleankod/exchange/entrypoint/AccountController.java
@@ -1,13 +1,14 @@
 package pl.cleankod.exchange.entrypoint;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import pl.cleankod.exchange.core.domain.Account;
 import pl.cleankod.exchange.core.usecase.AccountService;
 
 @RestController
 @RequestMapping("/accounts")
-public class AccountController {
+public class AccountController implements AccountApi {
 
     private final AccountService accountService;
 
@@ -15,13 +16,13 @@ public class AccountController {
         this.accountService = accountService;
     }
 
-    @GetMapping(path = "/{id}")
-    public ResponseEntity<Account> findAccountById(@PathVariable String id, @RequestParam(required = false) String currency) {
+    @Override
+    public ResponseEntity<Account> findAccountById(String id, String currency) {
         return accountService.findAccountById(id, currency);
     }
 
-    @GetMapping(path = "/number={number}")
-    public ResponseEntity<Account> findAccountByNumber(@PathVariable String number, @RequestParam(required = false) String currency) {
+    @Override
+    public ResponseEntity<Account> findAccountByNumber(String number, String currency) {
         return accountService.findAccountByNumber(number, currency);
     }
 
