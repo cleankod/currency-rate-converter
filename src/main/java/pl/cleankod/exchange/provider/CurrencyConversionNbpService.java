4e35ae573ffc722a55e2067de6dafcad70ecaf9e
saruc <saru.cristian.florin@gmail.com>
2024-02-28 13:59:12 +0200

    handle edge case when convertig from PLN account

--- src/main/java/pl/cleankod/exchange/provider/CurrencyConversionNbpService.java
+++ src/main/java/pl/cleankod/exchange/provider/CurrencyConversionNbpService.java
@@ -15,6 +15,8 @@ public class CurrencyConversionNbpService implements CurrencyConversionService {
     private final ExchangeRatesNbpClient exchangeRatesNbpClient;
     private final CurrencyConversionStubService currencyConversionStubService;
 
+    private final Currency baseCurrency = Currency.getInstance("PLN");
+
     public CurrencyConversionNbpService(ExchangeRatesNbpClient exchangeRatesNbpClient,CurrencyConversionStubService currencyConversionStubService) {
         this.exchangeRatesNbpClient = exchangeRatesNbpClient;
         this.currencyConversionStubService = currencyConversionStubService;
@@ -22,17 +24,29 @@ public class CurrencyConversionNbpService implements CurrencyConversionService {
 
     @Override
     public Money convert(Money money, Currency targetCurrency) {
-        //the API gives back the rate between the base currency which is ALWAYS PLN and targetCurrency for example USD
-        //for example if you want to convert from EUR TO USD
-        //first we must convert from EUR TO PLN
-        // then convert from PLN TO the target currency
 
-        BigDecimal amountFromAccountCurrencyToBaseCurrency = currencyConversionStubService.convert(money, Currency.getInstance("PLN")).amount();
+        //converting from PLN ACCOUNT to other currency
+        if(money.currency().getCurrencyCode().equalsIgnoreCase(baseCurrency.getCurrencyCode())) {
 
-        RateWrapper rateWrapper = exchangeRatesNbpClient.fetch("A", targetCurrency.getCurrencyCode());
-        BigDecimal midRateBetWeenTargetCurrencyAndBaseCurrency = rateWrapper.rates().get(0).mid().setScale(2, RoundingMode.HALF_EVEN);
-        BigDecimal targetCurrencyAmountFromBaseCurrency = amountFromAccountCurrencyToBaseCurrency.divide(midRateBetWeenTargetCurrencyAndBaseCurrency, RoundingMode.HALF_EVEN);
+            RateWrapper rateWrapper = exchangeRatesNbpClient.fetch("A", targetCurrency.getCurrencyCode());
+            BigDecimal midRate = rateWrapper.rates().get(0).mid().setScale(2, RoundingMode.HALF_EVEN);
+            return new Money(money.amount().divide(midRate, RoundingMode.HALF_EVEN), targetCurrency);
 
-        return new Money(targetCurrencyAmountFromBaseCurrency, targetCurrency);
+        } else {
+            //converting from currency that is not baseCurrency(PLN)
+            //the API gives back the rate between the base currency which is ALWAYS PLN and targetCurrency for example USD
+            //for example if you want to convert from EUR TO USD
+            //first we must convert from EUR TO PLN
+            // then convert from PLN TO the target currency
+
+            BigDecimal amountFromAccountCurrencyToBaseCurrency = currencyConversionStubService.convert(money, Currency.getInstance("PLN")).amount();
+
+            RateWrapper rateWrapper = exchangeRatesNbpClient.fetch("A", targetCurrency.getCurrencyCode());
+            BigDecimal midRateBetWeenTargetCurrencyAndBaseCurrency = rateWrapper.rates().get(0).mid().setScale(2, RoundingMode.HALF_EVEN);
+            BigDecimal targetCurrencyAmountFromBaseCurrency = amountFromAccountCurrencyToBaseCurrency.divide(midRateBetWeenTargetCurrencyAndBaseCurrency, RoundingMode.HALF_EVEN);
+
+            return new Money(targetCurrencyAmountFromBaseCurrency, targetCurrency);
+        }
+
     }
 }
